ff21f322837b255c5f0f7ca61244f7efd313f5af
Ali El-Ashram <ali.elashram@gmail.com>
2013-09-05 18:18:48 -0700

    Add favorite movie and release year to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   def home
   	@first_movie = Movie.first
   	@first_count = @first_movie.votes.count 
+
+  	@my_favorite = Movie.find_by title: 'The Message'
   end
 
 end
